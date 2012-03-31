48c57ab71f491b69f03d2f189486da17267522fe
balint <balint@693e1593-6c1f-0410-997a-f041c8489887>
2012-03-31 03:38:09 +0000

    Complex float output normalised to [-1.0f,1.0f) Error in Noxon ver 2: was incorrect tuner type (now E4000)

--- python/gen_char_to_float_lut.py
+++ python/gen_char_to_float_lut.py
@@ -4,8 +4,8 @@ import sys
 
 sys.stdout.write("{ ");
 for i in range(256):
-	f = float(i) - 128
-	sys.stdout.write("%.1ff" % (f))
+	f = (float(i) - 128.0) / 128.0
+	sys.stdout.write("%ff" % (f))
 	if i < 255:
 		sys.stdout.write(", ")
 print(" };")
