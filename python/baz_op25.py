44225ce7e7d71257ea385b57906bc597380b2528
Balint Seeber <balint256@gmail.com>
2015-09-19 17:26:26 -0700

    OP25: enhancements here to match matching enhancements in OP25 (mainly GR 3.7 and decryption support)

--- python/baz_op25.py
+++ python/baz_op25.py
@@ -22,12 +22,15 @@
 _verbose = True
 
 import math
-from gnuradio import gr, gru, op25 as _op25
+from gnuradio import gr, gru
+import op25
+from baz import message_callback
 
+fsk4 = None
 try:
     from gnuradio import fsk4   # LEGACY
     if _verbose:
-        print "Imported legacy fsk4"
+        print "[OP25] Imported legacy fsk4"
 except:
     pass
 
@@ -42,59 +45,74 @@ class op25_fsk4(gr.hier_block2):
         
         self.symbol_rate = SYMBOL_RATE
         
-        #print "Channel rate:", channel_rate
+        #print "[OP25] Channel rate:", channel_rate
         self.channel_rate = channel_rate
         self.auto_tune_msgq = auto_tune_msgq
         
+        self.auto_tune_message_callback = None
         if self.auto_tune_msgq is None:
             self.auto_tune_msgq = gr.msg_queue(2)
+            self.auto_tune_message_callback = message_callback.message_callback(self.auto_tune_msgq)
         
         # C4FM demodulator
-        #print "Symbol rate:", self.symbol_rate
+        #print "[OP25] Symbol rate:", self.symbol_rate
         try:
-            self.demod_fsk4 = _op25.fsk4_demod_ff(self.auto_tune_msgq, self.channel_rate, self.symbol_rate)
+            self.demod_fsk4 = op25.fsk4_demod_ff(self.auto_tune_msgq, self.channel_rate, self.symbol_rate)
             if _verbose:
-                print "Using new fsk4_demod_ff"
-        except:
+                print "[OP25] Using new fsk4_demod_ff"
+        except Exception, e:
+            print "[OP25] New fsk4_demod_ff not available:", e
             try:
                 self.demod_fsk4 = fsk4.demod_ff(self.auto_tune_msgq, self.channel_rate, self.symbol_rate)   # LEGACY
                 if _verbose:
-                    print "Using legacy fsk4.demod_ff"
-            except:
+                    print "[OP25] Using legacy fsk4.demod_ff"
+            except Exception, e:
+                print e
                 raise Exception("Could not find a FSK4 demodulator to use")
         
         self.connect(self, self.demod_fsk4, self)
 
 class op25_decoder_simple(gr.hier_block2):
-    def __init__(self, traffic_msgq=None, key=None):
+    def __init__(self, idle_silence=True, traffic_msgq=None, key=None, key_map=None, verbose=False):
         gr.hier_block2.__init__(self, "op25_decoder",
                               gr.io_signature(1, 1, gr.sizeof_float),
                               gr.io_signature(1, 1, gr.sizeof_float))
         
         self.traffic_msgq = traffic_msgq
         self.key = key
+        self.key_map = key_map
         
-        if self.traffic_msgq is None:
-            self.traffic_msgq = gr.msg_queue(2)
+        self.traffic_message_callback = None
         
         self.slicer = None
         try:
             levels = [ -2.0, 0.0, 2.0, 4.0 ]
-            self.slicer = _op25.fsk4_slicer_fb(levels)
-            self.p25_decoder = _op25.decoder_bf()
-            self.p25_decoder.set_msgq(self.traffic_msgq)
-            if _verbose:
-                print "Using new decoder_bf"
-        except:
+            self.slicer = op25.fsk4_slicer_fb(levels)
             try:
-                self.p25_decoder = _op25.decoder_ff(self.traffic_msgq)   # LEGACY
-                if _verbose:
-                    print "Using legacy decoder_ff"
+                self.p25_decoder = op25.decoder_bf(idle_silence=idle_silence, verbose=verbose)
             except:
+                print "[OP25] Extended decoder_bf constructor not available"
+                self.p25_decoder = op25.decoder_bf()    # Original style
+            if self.traffic_msgq is not None:
+                self.p25_decoder.set_msgq(self.traffic_msgq)
+            if _verbose:
+                print "[OP25] Using new decoder_bf"
+        except Exception, e:
+            print "[OP25] New decoder_bf not available:", e
+            try:
+                if self.traffic_msgq is None:
+                    self.traffic_msgq = gr.msg_queue(2)
+                    self.traffic_message_callback = message_callback.message_callback(self.traffic_msgq)
+                self.p25_decoder = op25.decoder_ff(self.traffic_msgq)   # LEGACY
+                if _verbose:
+                    print "[OP25] Using legacy decoder_ff"
+            except Exception, e:
+                print e
                 raise Exception("Could not find a decoder to use")
+
+        self.set_key_map(self.key_map)
         
-        if (self.key is not None) and (len(self.key) > 0): # Relates to key string passed in from GRC block
-            self.set_key(self.key)
+        self.set_key(self.key)
         
         if self.slicer:
             self.connect(self, self.slicer, self.p25_decoder)
@@ -104,22 +122,76 @@ class op25_decoder_simple(gr.hier_block2):
     
     def set_key(self, key):
         try:
-            if type(key) == str:
-                if len(key) == 0:	# FIXME: Go back into the clear
-                    #print "Cannot set key using empty string"
-                    return False
-                key = int(key, 16) # Convert from hex string
             if not hasattr(self.p25_decoder, 'set_key'):
-                print "This version of the OP25 decoder does not support decryption"
+                print "[OP25] This version of the OP25 decoder does not support decryption"
                 return False
-            self.p25_decoder.set_key(key)
+
+            l = self._convert_key_string(key)
+            if l is None:
+                return False
+            
+            self.p25_decoder.set_key(l)
             return True
         except Exception, e:
-            print "Exception while setting key:", e
+            print "[OP25] Exception while setting key:", e
             return False
 
+    def set_key_map(self, key_map):
+        try:
+            if not hasattr(self.p25_decoder, 'set_key_map'):
+                print "[OP25] This version of the OP25 decoder does not support decryption"
+                return False
+
+            if (key_map is None) or (len(self.key_map) == 0):
+                return False
+
+            _key_map = {}
+            for k in key_map.keys():
+                v = key_map[k]
+                l = self._convert_key_string(v, k)
+                if l is None:
+                    continue
+                _key_map[k] = l
+            
+            self.p25_decoder.set_key_map(_key_map)
+            return True
+        except Exception, e:
+            print "[OP25] Exception while setting key map:", e
+            return False
+
+    def _convert_key_string(self, key, kid=None):
+        if key is None:
+            return
+
+        kid_str = ""
+        if kid is not None:
+            kid_str = " 0x%04x" % (kid)
+
+        if type(key) != str:
+            print "[OP25] Key%s type %s unsupported: %s" % (kid_str, str(type(key)), str(key))
+            return
+
+        if len(key) == 0:
+            #print "[OP25] Key%s empty" % (kid_str)
+            return
+
+        if (len(key) % 2) == 1:
+            print "[OP25] Missing a nibble from key%s: %s" % (kid_str, str(key))
+            return
+
+        l = []
+        for i in range(len(key) / 2):
+            l += [int(key[i*2 + 0] + key[i*2 + 1], 16)]
+        return l
+
+    def set_idle_silence(self, on):
+        self.p25_decoder.set_idle_silence(on)
+
+    def set_logging(self, on):
+        self.p25_decoder.set_logging(on)
+
 class op25_decoder(gr.hier_block2):
-    def __init__(self, channel_rate, auto_tune_msgq=None, defer_creation=False, output_dibits=False, key=None, traffic_msgq=None):
+    def __init__(self, channel_rate, idle_silence=True, auto_tune_msgq=None, defer_creation=False, output_dibits=False, key=None, key_map=None, traffic_msgq=None, verbose=False):
         num_outputs = 1
         if output_dibits:
             num_outputs += 1
@@ -128,22 +200,24 @@ class op25_decoder(gr.hier_block2):
                               gr.io_signature(1, 1, gr.sizeof_float),
                               gr.io_signature(num_outputs, num_outputs, gr.sizeof_float))
         
-        self.symbol_rate = SYMBOL_RATE
+        #self.symbol_rate = SYMBOL_RATE
         
         #print "Channel rate:", channel_rate
         self.channel_rate = channel_rate
+        self.idle_silence = idle_silence
+        self.verbose = verbose
         self.auto_tune_msgq = auto_tune_msgq
         self.traffic_msgq = traffic_msgq
         self.output_dibits = output_dibits
         self.key = key
-        self.traffic_msgq = gr.msg_queue(2)
+        self.key_map = key_map
         
         if defer_creation == False:
             self.create()
     
     def create(self):
         self.fsk4 = op25_fsk4(channel_rate=self.channel_rate, auto_tune_msgq=self.auto_tune_msgq)
-        self.decoder = op25_decoder_simple(traffic_msgq=self.traffic_msgq, key=self.key)
+        self.decoder = op25_decoder_simple(idle_silence=self.idle_silence, traffic_msgq=self.traffic_msgq, key=self.key, key_map=self.key_map, verbose=self.verbose)
         
         # Reference code
         #self.decode_watcher = decode_watcher(self.op25_msgq, self.traffic)
@@ -183,20 +257,16 @@ class op25_decoder(gr.hier_block2):
             self.connect(self.fsk4, (self, 1))
     
     def set_key(self, key):
-        try:
-            if type(key) == str:
-                if len(key) == 0:	# FIXME: Go back into the clear
-                    #print "Cannot set key using empty string"
-                    return False
-                key = int(key, 16) # Convert from hex string
-            if not hasattr(self.p25_decoder, 'set_key'):
-                print "This version of the OP25 decoder does not support decryption"
-                return False
-            self.p25_decoder.set_key(key)
-            return True
-        except Exception, e:
-            print "Exception while setting key:", e
-            return False
+        return self.decoder.set_key(key)
+
+    def set_key_map(self, key_map):
+        return self.decoder.set_key_map(key_map)
+
+    def set_idle_silence(self, on):
+        self.decoder.set_idle_silence(on)
+
+    def set_logging(self, on):
+        self.decoder.set_logging(on)
     
     # Reference code
     #def adjust_channel_offset(self, delta_hz):
